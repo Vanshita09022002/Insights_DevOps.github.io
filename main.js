b7355fc63e66a36dbe4a7f01269e006e89fd3441
Vanshita <128284421+Vanshita09022002@users.noreply.github.com>
2023-07-31 07:29:14 +0530

    Add files via upload
    index.html file added.

---
+++ main.js
@@ -0,0 +1,37 @@
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 0)
+})
+
+const issues = document.querySelectorAll('.issue');
+
+issues.forEach(issue => {
+    issue.addEventListener('click', () => {
+        issue.classList.toggle('open');
+
+        const icon = issue.querySelector('.issue__icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus";
+        } else {
+            icon.className = "uil uil-plus";
+        }
+    })
+})
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
